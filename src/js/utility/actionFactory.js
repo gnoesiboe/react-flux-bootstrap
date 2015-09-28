9f760742a957f461b66f67f63dd08e5b30a17417
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2015-09-28 06:26:54 +0200

    Initial commit

---
+++ src/js/utility/actionFactory.js
@@ -0,0 +1,20 @@
+var _ = require('underscore');
+
+/**
+ * @type {Object}
+ *
+ * @private
+ */
+var _blueprint = {
+    type: null
+};
+
+/**
+ * @type {Object}
+ */
+var actionFactory = {
+
+    //@todo
+};
+
+module.exports = actionFactory;
